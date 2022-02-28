d66774030e529cbbbd5c2fb16f4886fa10f2437f
Quu <91326783+Quu23@users.noreply.github.com>
2022-02-28 20:02:18 +0900

    graphics使ってみたー！

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import tkinter;
-import random;
+import graphics;
+
+global g_nowSetColor;
 
 
-      
+#main関数です。
 def main():
-    root = tkinter.Tk();
-    root.title("MIKOTO");
-    root.mainloop();
+    
+    g_nowSetColor = "green"
+    
+    Root = tkinter.Tk();
+    Root.title("MIKOTO");
+    Root.geometry("400x300");
+    
+    gra = graphics.Graphics(tkinter.Canvas(Root,width=400,height=300));
+    
+    gra.fillRect(100,100,50,50);
+    
+    
+    Root.mainloop();
+    
+
+
 
 if __name__ == "__main__" :
     main();
