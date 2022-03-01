9a22f20afc91dc6e66e455a7d31c5401f5c46304
Quu <91326783+Quu23@users.noreply.github.com>
2022-03-01 20:36:51 +0900

    ウィンドウを真ん中に表示できるようにした

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-import tkinter;
-import graphics;
+import tkinter
+import graphics
+import gameMode as mode
 
-global g_nowSetColor;
+global g_gra;
 
+flg =  mode.GameMode.START; 
+
+def gameLoop(root):
+    #TODO:ゲームの処理を書く
+    
+    #TODO:ループ条件をかく
+    
+    root.after(50,gameLoop);
 
 #main関数です。
 def main():
-    
-    g_nowSetColor = "green"
-    
+        
+    # ウィンドウの生成
     Root = tkinter.Tk();
-    Root.title("MIKOTO");
-    Root.geometry("400x300");
+    Root.title("Shooting-python");
     
-    gra = graphics.Graphics(tkinter.Canvas(Root,width=400,height=300));
+    #ウィンドウを真ん中に表示する処理
+    Root.geometry("{}x{}+{}+{}".format(600,500,int(Root.winfo_screenwidth()/2-300),int(Root.winfo_screenheight()/2-250)));
     
-    gra.fillRect(100,100,50,50);
+    # グラフィクスオブジェクトの生成
+    g_gra = graphics.Graphics(tkinter.Canvas(Root,width=600,height=500,bg="white"));
     
+    g_gra.fillRect(10,30,100,100);
     
+    # メインループの実行
     Root.mainloop();
     
-
-
 
 if __name__ == "__main__" :
     main();
