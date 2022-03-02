afa797bdd6ecbb24b6ad94429e9de4ba061a8f40
Quu <91326783+Quu23@users.noreply.github.com>
2022-03-02 20:03:13 +0900

    ゲームループ内でgraphicsが使えるようにした！

--- main.py
+++ main.py
@@ -2,32 +2,38 @@ import tkinter
 import graphics
 import gameMode as mode
 
-global g_gra;
+# ウィンドウの生成
+Root = tkinter.Tk();
+# グラフィクスオブジェクトの生成
+g_gra = graphics.Graphics(tkinter.Canvas(Root,width=600,height=500,bg="white"));
 
 flg =  mode.GameMode.START; 
 
-def gameLoop(root):
+def gameLoop():
     
     #TODO:ゲームの処理を書く
     
+    g_gra.fillRect(10,10,10,10);
+    
+    
+    
+    g_gra.clear();
     #TODO:ループ条件をかく
     
-    root.after(50,gameLoop);
+    
 
 #main関数です。
 def main():
+    
         
-    # ウィンドウの生成
-    Root = tkinter.Tk();
     Root.title("Shooting-python");
     
     #ウィンドウを真ん中に表示する処理
     Root.geometry("{}x{}+{}+{}".format(600,500,int(Root.winfo_screenwidth()/2-300),int(Root.winfo_screenheight()/2-250)));
     
-    # グラフィクスオブジェクトの生成
-    g_gra = graphics.Graphics(tkinter.Canvas(Root,width=600,height=500,bg="white"));
     
-    g_gra.fillRect(10,30,100,100);
+
+    Root.after(50,gameLoop());
     
     # メインループの実行
     Root.mainloop();
