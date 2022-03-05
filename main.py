97d54076cce474b7b79e70fbfd445583778f95b5
Quu <91326783+Quu23@users.noreply.github.com>
2022-03-05 20:48:42 +0900

    keyイベント処理を書いた

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 import tkinter
-from turtle import goto
 import graphics
 import gameMode as mode
 
@@ -12,19 +11,62 @@ gra = graphics.Graphics(tkinter.Canvas(Root,width=600,height=500,bg="white"));
 gameFlg =  mode.GameMode.START; 
 
 images = [tkinter.PhotoImage(file="img\Quu.png"),];
+#           key.A key.D Key.W Key.S Key.K Key.Enter
+pushKeys = [False,False,False,False,False,False];
 
+def keyPressed(event):
+    
+    pushKeyCode = event.keysym
 
+    match(pushKeyCode):
+        case "a":
+            pushKeys[0]=True;
+        case "d":
+            pushKeys[1]=True;
+        case "w":
+            pushKeys[2]=True;
+        case "s":
+            pushKeys[3]=True;
+        case "k":
+            pushKeys[4]=True;
+        case "Return":
+            pushKeys[5]=True;
+            
+def keyReleased(event):
+    
+    pushKeyCode = event.keysym
+
+    match(pushKeyCode):
+        case "a":
+            pushKeys[0]=False;
+        case "d":
+            pushKeys[1]=False;
+        case "w":
+            pushKeys[2]=False;
+        case "s":
+            pushKeys[3]=False;
+        case "k":
+            pushKeys[4]=False;
+        case "Return":
+            pushKeys[5]=False;
+    
+            
 def gameLoop():
+    
+    global gameFlg;
     
     gra.clear();
     
     #TODO:ゲームの処理を書く
     match(gameFlg):
-    
+        
         case mode.GameMode.START:
-            gra.fillOval(100,100,100,100);
-            gra.drawText(100,200,"test",30);
-            gra.fillRect(0,0,300,10);
+            gra.drawText(300,150,"Shooting",100);
+            gra.drawText(300,250,"Push Enter to Play",50);
+            
+            if(pushKeys[5]):
+                gameFlg=mode.GameMode.GAME;
+                
         case mode.GameMode.GAME:
             print();
         case mode.GameMode.BOSS:
@@ -49,6 +91,9 @@ def main():
     
     #ウィンドウを真ん中に表示する処理
     Root.geometry("{}x{}+{}+{}".format(600,500,int(Root.winfo_screenwidth()/2-300),int(Root.winfo_screenheight()/2-250)));
+    
+    Root.bind("<KeyPress>", keyPressed);
+    Root.bind("<KeyRelease>", keyReleased)
     
     sys.setrecursionlimit(2000);
 
