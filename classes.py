44e609be2d89bdb18ae5fef16e3b219f0f0c640b
Quu <91326783+Quu23@users.noreply.github.com>
2022-03-07 07:31:27 +0900

    弾を打てるようにした

--- classes.py
+++ classes.py
@@ -1,18 +1,19 @@
-
+import random;
 class Plane:
     
-    def __init__(self,x,y,hp):
+    def __init__(self,x,y,hp,color):
         self.bullets = [];
         self.x = x;
         self.y = y;
         self.hp=hp;
         self.reloadTime = 0;
+        self.color = color;
         
     def addBullet(self):
         self.bullets.append(Bullet(self.x+10,self.y+5));
     
     def checkHit(self,object):
-        if (self.x - object.x)**2+(self.y-object.y)**2 <150 :
+        if (self.x - object.x)**2+(self.y-object.y)**2 <200 :
             return True;
         return False;
     
@@ -25,8 +26,8 @@ class Plane:
                 
             
 class Player(Plane):
-    def __init__(self, x, y,hp):
-        super().__init__(x, y,hp);
+    def __init__(self, x, y):
+        super().__init__(x, y,10,"blue");
         self.moveSpeed = 10;
     
     def moveBullet(self):
@@ -45,8 +46,18 @@ class Player(Plane):
     
 
 class Enemy(Plane):
-    def __init__(self, x, y,hp):
-        super().__init__(x, y,hp);
+    
+    def __init__(self, x, y):
+        super().__init__(x, y,5,"red");
+        self.size = 10;
+    
+    def action(self):
+        if random.randint(1,5) > 2  and self.reloadTime == 0:
+            self.addBullet();
+            self.reloadTime = 10;
+        else:
+            self.move();
+            self.reloadTime -= 1;
     
     def move(self):
         
@@ -63,7 +74,7 @@ class Enemy(Plane):
             i -= count ;
             
             if self.bullets[i].x >0:
-                self.bullets[i].x -= 3;
+                self.bullets[i].x -= 7;
             else:
                 self.bullets.pop(i);
                 count += 1; 
@@ -74,16 +85,25 @@ class Enemy(Plane):
         return False;
     
             
-        
+class miniBoss(Enemy):
+    def __init__(self, x, y):
+        super().__init__(x, y);
+        self.hp = 15;
+        self.color = "green";
+        self.size = 15;
         
 class Boss(Enemy):    
-    def __init__(self, x, y,hp):
-        super().__init__(x, y,hp);
+    def __init__(self, x, y):
+        super().__init__(x, y);
+        self.hp = 50;
+        self.color = "black";
+        self.size = 30;
     
 class Bullet:
     def __init__(self,x,y):
         self.x = x;
         self.y = y;
+        
         
     
 
